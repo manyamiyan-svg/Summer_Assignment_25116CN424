39ed08446f9607050ff608210428890d45adde7e
manyamiyan-svg <manyamiyan@gmail.com>
2026-05-28 21:26:59 +0530

    Create Sum.c

---
+++ Day-1/Sum.c
@@ -0,0 +1,13 @@
+int main() {
+    int n,i,sum=0;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++){
+        sum=sum+i;
+}
+        printf("sum of %d natural nos=%d",n,sum);
+        
+    
+
+    return 0;
+}
