1559b026bf3ac3ef8966a0e84a3630b5245cd14c
manyamiyan-svg <manyamiyan@gmail.com>
2026-05-28 21:33:14 +0530

    Create Mul.c

---
+++ Day-1/Mul.c
@@ -0,0 +1,11 @@
+int main() {
+    int n,i,mul=1;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+        mul=n*i;
+
+  printf("%d*%d=%d\n",n,i,mul);
+}
+
+    return 0;
