4f073ca9291245cd0c043f792d10bedee0cf6a9a
manyamiyan-svg <manyamiyan@gmail.com>
2026-05-31 21:57:40 +0530

    Add Armstrong number check in Armstrong.c

---
+++ Day-2/Day-4/Armstrong.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,n,sum=0,count=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    int og=n;
+    while (n!=0){
+        count++;
+        n=n/10;
+    }
+    n=og;
+    // calculating the sum for checking armstrong number
+    while (n!=0){
+        a=n%10;
+        sum = sum+ pow(a,count);
+        n=n/10;
+    }
+    if (sum==og){
+        printf("the number is an armstrong number");
+    }
+    else{
+        printf("the number is not an armstrong number");
+    }
+    return 0;
+}
