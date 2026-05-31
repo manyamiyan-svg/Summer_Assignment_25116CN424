a6d6088d09692e835f4c541b919f2ca89b4164c0
manyamiyan-svg <manyamiyan@gmail.com>
2026-05-31 22:00:54 +0530

    Create Armstrongrange.c

---
+++ Day-2/Day-4/Armstrongrange.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int start, end, num, temp, rem, digits, sum;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    for(num = start; num <= end; num++)
+    {
+        temp = num;
+        digits = 0;
+
+        while(temp != 0)
+        {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+        sum = 0;
+
+        while(temp != 0)
+        {
+            rem = temp % 10;
+            sum += pow(rem, digits);
+            temp /= 10;
+        }
+
+        if(sum == num)
+            printf("%d ", num);
+    }
+
+    return 0;
+}
