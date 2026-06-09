573a725e981110a4bff835fd80c899dece4d124b
manyamiyan-svg <manyamiyan@gmail.com>
2026-06-09 21:54:33 +0530

    Create repeatedcharacter.c

---
+++ Day-9/repeatedcharacter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    printf("Enter no. of rows:\n");
+    scanf("%d", &n);
+    printf("Repeated character pyramid with %d rows:\n", n);
+    char x = 'A';
+    for ( i = 1; i <= n; i++) {
+        for ( j = 1; j <= i; j++) {
+            printf("%c ", x);
+        }
+        x++;
+        printf("\n");
+    }
+    return 0;
+}
