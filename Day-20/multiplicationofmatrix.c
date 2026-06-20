5aa371e34a9711c52e6bc9b7cbc897d01d324fe9
manyamiyan-svg <manyamiyan@gmail.com>
2026-06-20 22:50:54 +0530

    Create multiplicationofmatrix.c

---
+++ Day-20/multiplicationofmatrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main() {
+    int r1, c1, r2, c2,i,j,k,m1[50][50],m2[50][50],res[50][50];
+    printf("Enter no. of rows and columns of 1st matrix:\n");
+    scanf("%d %d", &r1, &c1);
+    printf("Enter no. of rows and columns of 2nd matrix:\n");
+    scanf("%d %d", &r2, &c2);
+    if (r1 < 1 || c1 < 1 || r2 < 1 || c2 < 1) {
+        printf("Invalid length. Cannot be negative or 0.\n");
+        return 0;
+    }
+    if (c1 != r2) {
+        printf("Multiplication not possible due to improper orders.\n");
+        return 0;
+    }
+    printf("Enter %d elements for 1st matrix:\n", r1 * c1);
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++)
+            scanf("%d", &m1[i][j]);
+    printf("Enter %d elements for 2nd matrix:\n", r2 * c2);
+    for (i = 0; i < r2; i++)
+        for (j = 0; j < c2; j++)
+            scanf("%d", &m2[i][j]);
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            res[i][j] = 0;
+            for (k = 0; k < c1; k++) { 
+                res[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    printf("Multiplication of matrices is:\n");
+    for (i = 0; i < r1; i++) {
+  for (j = 0; j < c2; j++)
+            printf("%d ", res[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
